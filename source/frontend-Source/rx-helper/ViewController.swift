9b59369ff2d697fd01f34e6ac97df9e6ca4aa7c1
Juan Brena <brenaj@unlv.nevada.edu>
2018-09-19 15:08:09 -0700

    Add xcode files

---
+++ source/frontend-Source/rx-helper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rx-helper
+//
+//  Created by Work-Account on 9/18/18.
+//  Copyright © 2018 cs477Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
