2bc3ae0e297b40fb660d51781acc74b05329353c
Juan Brena <brenaj@unlv.nevada.edu>
2018-10-21 00:36:02 -0700

    Display logged-in user's name in HomeVC

--- source/rx-helper/HomeViewController.swift
+++ source/rx-helper/HomeViewController.swift
@@ -13,10 +13,13 @@ import FirebaseDatabase
 
 class HomeViewController: BaseViewController {
 
+    @IBOutlet weak var HomeNavBar: UINavigationItem!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view
         addSlideMenuButton()
+        HomeNavBar.title = getUserDisplayName()
     }
 
 
@@ -26,4 +29,20 @@ class HomeViewController: BaseViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    //This function will return the name of the user that is logged in
+    //We will need to modify this to change in case the person decides
+    //to change profile
+    func getUserDisplayName() -> String {
+        let user = Auth.auth().currentUser
+        if user != nil {
+            if user?.displayName != nil {
+                return (user?.displayName!)!
+            }
+            else{
+                return("")
+            }
+        }
+        return ("Error: no user logged in")
+    }
+
 }
