781a0d01a258804cc25665d6df1ce9b561101adc
Juan Brena <brenaj@unlv.nevada.edu>
2018-10-29 13:54:49 -0700

    Add delete option to side menu, add AddUserVC

---
+++ source/rx-helper/AddUserViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AddUserViewController.swift
+//  rx-helper
+//
+//  Created by Juan Brena on 10/29/18.
+//  Copyright © 2018 cs477Team. All rights reserved.
+//
+
+import UIKit
+
+class AddUserViewController: UIViewController {
+
+    @IBOutlet weak var newUsersName: UITextField!
+    @IBAction func addNewUserButton(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- source/rx-helper/MenuViewController.swift
+++ source/rx-helper/MenuViewController.swift
@@ -8,22 +8,80 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseDatabase
+
+var users = ["Juan Brena","Nick Graeff","Ajanae Williams","Alexis Acosta","Jean-Paul Castro","Eduardo Meza","Blossom Hamika"]
 
 protocol SlideMenuDelegate {
     func slideMenuItemSelectedAtIndex(_ index: Int32)
 }
 
 class MenuViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MenuTableViewCell") as! MenuTableViewCell
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
         
-        return cell
+    }
+
+
+    //setting the number of cells in the table View
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (users.count + 1)
     }
     
+    //setting the text for each cell in the table View
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        if (indexPath.item < users.count){
+            let cell = tableView.dequeueReusableCell(withIdentifier: "MenuTableViewCell", for: indexPath)
+            cell.textLabel?.text = users[indexPath.row]
+            
+            return cell
+        }
+        else{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "MenuTableViewCell", for: indexPath)
+            cell.textLabel?.text = "Add user +"
+            
+            return cell
+        }
+    }
+    
+    //setting the size of the cells in the table View
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 35
+    }
+    
+    // method to run when table view cell is tapped
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print("You tapped cell number \(indexPath.row).")
+        if (indexPath.row == users.count){
+            print("To Add User VC")
+            self.performSegue(withIdentifier: "toAddUser", sender: nil)
+        }
+        else{
+            print("To Home VC")
+            self.performSegue(withIdentifier: "toHome", sender: nil)
+        }
+    }
+    
+    // method to handle row deletion
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if (indexPath.row != users.count) {
+            if editingStyle == .delete {
+                
+                //remove the item from the data model
+                users.remove(at: indexPath.row)
+                
+                //delete the table view row
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+            else if editingStyle == .insert{
+                //Not used in our example, but if you were adding a new row, this is where you would do it
+            }
+        }
+    }
 
     @IBAction func logoutButton(_ sender: Any) {
         let alert = UIAlertController(title: "Log out of " + getUserDisplayName() + "?", message: nil, preferredStyle: .alert)
@@ -33,17 +91,11 @@ class MenuViewController: UIViewController, UITableViewDelegate, UITableViewData
 
         self.present(alert, animated: true)
     }
+    
     var btnMenu: UIButton!
     var delegate: SlideMenuDelegate?
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-
-    }
-
     @IBOutlet weak var btnCloseMenuOverlay: UIButton!
-
     @IBAction func onCloseMenuClick(_ button: UIButton!) {
         btnMenu.tag = 0
 
